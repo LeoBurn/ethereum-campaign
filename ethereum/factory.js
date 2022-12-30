72deb5ba76e43199dc7b9e91ad86c417fd611b2c
Leandro Viana <LeandroViana1988@gmail.com>
2022-12-30 18:49:09 +0000

    Update GitIgnore and factory

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,6 +1,7 @@
 import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
+
 const intance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
     process.env.FACTORY_ADDRESS
